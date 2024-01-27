8c2e1ce1314c71e0fbe70bca79fa36a6357989d0
BarrGrace <barr.grace@grunitech.com>
2024-01-27 02:42:17 +0200

    two ways to create a transaction

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,16 +1,16 @@
-pragma solidity >=0.4.22 <0.7.0;
+pragma solidity >=0.4.22 <0.9.0;
 
 contract Migrations {
-  address public owner;
+  address payable public owner;
   uint256 public last_completed_migration;
 
    modifier restricted() {
     if (msg.sender == owner) _;
   }
 
-  constructor() public {
-    owner = msg.sender;
-  }
+  // constructor() public {
+  //   owner = msg.sender;
+  // }
 
   function setCompleted(uint completed) public restricted {
     last_completed_migration = completed;
@@ -21,15 +21,31 @@ contract Migrations {
     upgraded.setCompleted(last_completed_migration);
   }
 
-  function getter() public view returns(uint256) {
-    return last_completed_migration;
-  }
+  // function setter(uint256 new_completed_migration) public {
+  //   last_completed_migration = new_completed_migration;
+  // }
 
-  function setter(uint256 new_completed_migration) public {
-    last_completed_migration = new_completed_migration;
-  }
+  // function balanceOfContract() public view returns(uint) {
+  //   return address(this).balance;
+  // }
 
-  function balanceOfContract() public view returns(uint) {
-    return address(this).balance;
+  // function getter(uint amount) public view returns(value) {
+  //   return address(this).value;
+  // }
+    event ValueChanged(uint oldValue, uint newValue);
+
+    function changeValue() external payable {
+        uint oldValue = msg.value;
+
+        // Change the value (for example, double it)
+        uint newValue = oldValue * 2;
+
+        // Emit an event to log the change
+        emit ValueChanged(oldValue, newValue);
+    }
+
+  function sendEther(address payable _to) public payable{
+    bool sent = _to.send(msg.value);
+    require(sent, "Fail!");
   }
 }
--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -29,25 +29,27 @@ export class AccountService {
     ///
 
     const RPC = "HTTP://127.0.0.1:7545"
-    const provider = new JsonRpcProvider(RPC)
+    const provider = new JsonRpcProvider(RPC) 
     
     const network_id = parseInt((await provider.getNetwork()).chainId.toString());
     const network_address = migration.networks[network_id].address
 
-    // const singer = await provider.getSigner(0);
+    const singer = await provider.getSigner(0); 
+    const contracts = new ethers.Contract(network_address, migration.abi, singer);
+    await contracts.changeValue({
+      value: ethers.parseEther('10')
+    })
+    
+    contracts.sendEther("0xf1dAC20B7c8F6A5013CE07012db023866C7A6133").then(() => console.log("Sent"));
 
-    // const contracts = new ethers.Contract(network_address, migration.abi, singer);
-    // contracts.balanceOfContract().then(e => console.log("the balance is", e));
 
-
-
-    const singer = await provider.getSigner(0);
-    const transaction = {
-      from: "0x233Ca9680Ec827232D24efc2fF333aB74c377E9d",
-      to: "0xf1dAC20B7c8F6A5013CE07012db023866C7A6133",
-      value: ethers.parseEther("9")
-    }
-    singer.sendTransaction(transaction).then(() => console.log("Send transaction"))
+    // const singer = await provider.getSigner("0xf1dAC20B7c8F6A5013CE07012db023866C7A6133");
+    // const transaction = {
+    //   from: "0xf1dAC20B7c8F6A5013CE07012db023866C7A6133",
+    //   to: "0x233Ca9680Ec827232D24efc2fF333aB74c377E9d",
+    //   value: ethers.parseEther("10")
+    // }
+    // singer.sendTransaction(transaction).then(() => console.log("Send transaction"))
 
     // return this.accountRepository.save(account); 
   }
